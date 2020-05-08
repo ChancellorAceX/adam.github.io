6fd795ea4222cb39664df6e14459e123ca58ca1b
Adam Brown <adam.edward.brown@gmail.com>
2020-05-08 17:55:12 -0400

    revision 1

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const handleAbout = function(){
           I'm a recent graduate from the Engineering Immersion bootcamp at Thinkful where I learned how to implement various programming technologies and methodologies. I believe learning how to implement these new technologies, along with my background in Biomedical Engineering I received from Georgia Tech, makes me a valuable member of any team. These combined educations allows me a unique perspective on the process that most developers won't have. I understand the importance of continually thinking about the end user as a project is being developed, and all the complications that implies. That being said, I love problem solving, and I feel like I have finally found my niche to allow my unique skills to flourish.
         </p>
         <p class=about>
-          Outside of coding I have several interests. I'm a DnD Dungion Master and love playing all sorts of tabletop games. I'm a percussionist, avid video gamer (primarily RPG's), comic book fan, TV/movie enthusiast, and an all around general nerd. Every year you can find me at DragonCon either nerding out on one of my interests, messing with people as Deadpool, or just wandering around having a good time.
+          Outside of coding I have several interests. I'm a DnD Dungeon Master and love playing all sorts of tabletop games. I'm a percussionist, avid video gamer (primarily RPG's), comic book fan, TV/movie enthusiast, and an all around general nerd. Every year you can find me at DragonCon either nerding out on one of my interests, messing with people as Deadpool, or just wandering around having a good time.
         </p>
       </section>`);
   });
